a630530f35f751007edd6850b9ddc88ac0a1fab3
張議隆 <nick900828@gmail.com>
2020-04-10 20:30:34 +0800

    Added level map data

--- level_1.h
+++ level_1.h
@@ -2,15 +2,21 @@
 #define LEVEL_1_H
 
 #include <QObject>
+using namespace std;
 class level_1 : public QObject
 {
     Q_OBJECT
 public:
     explicit level_1(QObject *parent = nullptr);
-
+    struct array {short int map[9][9];};
+    array arr;
+    static array const arrdata;
 signals:
 
 public slots:
+
+private:
+
 };
 
 #endif // LEVEL_1_H
