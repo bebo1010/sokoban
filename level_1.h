38a8581034f898df9627cd9677310da43f51fc73
張議隆 <nick900828@gmail.com>
2020-05-04 16:49:18 +0800

    Update level_1.h

--- level_1.h
+++ level_1.h
@@ -2,21 +2,31 @@
 #define LEVEL_1_H
 
 #include <QObject>
-#include <QMessageBox>
-#include "mainwindow.h"
-class level_1 : public QObject
-{
+using namespace std;
+class level_1 : public QObject {
     Q_OBJECT
-public:
+  public:
+
     explicit level_1(QObject *parent = nullptr);
+    short int const arrdata[10][10]= {
+        {0, 2, 2, 2, 2, 0, 0, 0, 0, 0},
+        {0, 2, 1, 1, 2, 2, 2, 2, 2, 0},
+        {0, 2, 4, 1, 1, 1, 4, 1, 2, 0},
+        {0, 2, 1, 3, 2, 3, 1, 1, 2, 0},
+        {2, 2, 1, 2, 2, 2, 1, 2, 2, 0},
+        {2, 1, 4, 3, 2, 3, 1, 2, 0, 0},
+        {2, 1, 5, 1, 1, 1, 4, 2, 0, 0},
+        {2, 2, 2, 2, 2, 1, 1, 2, 0, 0},
+        {0, 0, 0, 0, 2, 2, 2, 2, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
 
+  signals:
 
-private:
+  public slots:
+    void mapGen();
+  private:
 
-
-signals:
-
-public slots:
 };
 
 #endif // LEVEL_1_H
