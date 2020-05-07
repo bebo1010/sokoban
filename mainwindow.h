c70e3ee25a744a48ffa12f7b2ef6fdc73d69ce1e
張議隆 <nick900828@gmail.com>
2020-05-07 15:05:25 +0800

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -9,71 +9,49 @@
 #include <QPainter>
 #include <QRectF>
 #include <QPainter>
-#include "level_1.h"
+#include <QMessageBox>
+
+#include <QPushButton>
+#include <QApplication>
+#include <QFont>
+
+#include <QString>
+#include <QLineEdit>
+#include <QFile>
+#include <QDataStream>
 namespace Ui {
-    class MainWindow;
+class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
-public:
-    void mapGen(short int const arr[10][10]);
-    void init(short int const lvl[10][10]);
+  public:
     explicit MainWindow(QWidget *parent = nullptr);
+    void mainmenu();
+
+    void input_map_name();
+    void load_map();
+    void mapGen(short int (&arrdata)[10][10]);
+    void init(short int (&arr)[10][10]);
+
     void character_turn_back();
     void character_turn_front();
     void character_turn_left();
     void character_turn_right();
+
     void drawBackground(QPainter *painter, const QRectF &rect);
 
     void checkWin();
     void checkDead();
-
     void clear_map();
     void close();
 
     ~MainWindow();
-protected:
-    void paintEvent(QPaintEvent *event);
-private:
-    int win=0;
-    Ui::MainWindow *ui;
-    short int const arrdata2[10][10] = {
-        {2 , 2 , 2 , 2 , 2 , 2 , 0 , 0 , 0 ,0},
-        {2 , 1 , 1 , 1 , 1 , 2 , 2 , 0 , 0 ,0},
-        {2 , 3 , 4 , 4 , 3 , 1 , 2 , 2 , 0 ,0},
-        {2 , 1 , 3 , 4 , 4 , 3 , 1 , 2 , 2 ,0},
-        {2 , 2 , 1 , 3 , 4 , 4 , 3 , 1 , 2 ,0},
-        {0 , 2 , 2 , 1 , 3 , 4 , 4 , 3 , 2 ,0},
-        {0 , 0 , 2 , 2 , 1 , 5 , 1 , 1 , 2 ,0},
-        {0 , 0 , 0 , 2 , 2 , 2 , 2 , 2 , 2 ,0},
-        {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,0},
-        {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,0}
-                                        };
-     short int const arrdata1[10][10]={
-        {0 , 2 , 2 , 2 , 2 , 0 , 0 , 0 , 0 , 0},
-        {0 , 2 , 1 , 1 , 2 , 2 , 2 , 2 , 2 , 0},
-        {0 , 2 , 4 , 1 , 1 , 1 , 4 , 1 , 2 , 0},
-        {0 , 2 , 1 , 3 , 2 , 3 , 1 , 1 , 2 , 0},
-        {2 , 2 , 1 , 2 , 2 , 2 , 1 , 2 , 2 , 0},
-        {2 , 1 , 4 , 3 , 2 , 3 , 1 , 2 , 0 , 0},
-        {2 , 1 , 5 , 1 , 1 , 1 , 4 , 2 , 0 , 0},
-        {2 , 2 , 2 , 2 , 2 , 1 , 1 , 2 , 0 , 0},
-        {0 , 0 , 0 , 0 , 2 , 2 , 2 , 2 , 0 , 0},
-        {0 , 0 , 0 , 0 , 2 , 2 , 2 , 2 , 0 , 0}};
 
-     short int const arrdata[10][10]={
-        {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0},
-        {0 , 0 , 1 , 1 , 0 , 0 , 0 , 0 , 0 , 0},
-        {0 , 0 , 4 , 1 , 1 , 1 , 4 , 1 , 0 , 0},
-        {0 , 0 , 1 , 3 , 0 , 3 , 1 , 1 , 0 , 0},
-        {0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0},
-        {0 , 1 , 4 , 3 , 0 , 3 , 1 , 0 , 0 , 0},
-        {0 , 1 , 5 , 1 , 1 , 1 , 4 , 0 , 0 , 0},
-        {0 , 0 , 0 , 0 , 0 , 1 , 1 , 0 , 0 , 0},
-        {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0},
-        {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0}};
+    int idx_ground;
+    int idx_wall;
+    int idx_target;
+    int idx_box;
 
     QPixmap* ground[100];
     QPixmap* wall[100];
@@ -86,14 +64,8 @@ private:
     QLabel* Target[100];
     QLabel* Box[100];
     QLabel* Player[1];
-
     int px;
     int py;
-    int steps=0;
-    int idx_ground;
-    int idx_wall;
-    int idx_target;
-    int idx_box;
 
     QLabel *mainplayer;
     QLabel *player_facing;
@@ -103,6 +75,34 @@ private:
     QPixmap *back;
     QPixmap *left;
     QPixmap *right;
+
+    QLabel * setLabel(QLabel *label);
+  public slots:
+
+    void map_preprocessor();
+    void quit();
+    void hide_menu();
+  protected:
+    //void paintEvent(QPaintEvent *event);
+  private:
+    Ui::MainWindow *ui;
+    QLabel *mainmenu_BG;
+    QPixmap *mainmenu_image;
+    bool first_run = 0;
+    int steps=0;
+
+    QLineEdit *input_level;
+    char level_count = '1';
+    QPushButton *Input_btn;
+    QLabel *Input_Rule;
+    QString loaded_level = ":/mapdata/level_";
+    short int level_data[10][10];
+
+
+    QPushButton *Start_btn;
+    QPushButton *Exit_btn;
+
+
     void keyPressEvent( QKeyEvent *event );
 };
 
